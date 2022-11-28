79b993fc76e70cfacc9e5c06b14f76ec5f677021
joaquimmulaza <joaquimmulaza@gmail.com>
2022-11-28 16:19:18 +0100

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/* let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+} */
+
+// Typing Script JS
+var typed = new Typed(".typing",{
+    strings: ["Seja bem vindo ao Hospital Web"],
+    typeSpeed: 80,
+    backSpeed: 80,
+});
+
